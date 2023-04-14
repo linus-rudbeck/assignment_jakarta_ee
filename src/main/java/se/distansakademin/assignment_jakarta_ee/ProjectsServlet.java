a3320a9ebea7b3cc770e6a15d98076c9b4d1c888
linus-rc <linus.rudbeck@distansakademin.se>
2023-04-14 13:01:02 +0200

    Comments prep

--- src/main/java/se/distansakademin/assignment_jakarta_ee/ProjectsServlet.java
+++ src/main/java/se/distansakademin/assignment_jakarta_ee/ProjectsServlet.java
@@ -22,23 +22,37 @@ public class ProjectsServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Get current action from url
+        // String action = request.getParameter("action");
+        // action = (action == null) ? "create" : action;
+
         String title = request.getParameter("title");
         String description = request.getParameter("description");
 
+
+        // if (action.equals("create")) {
         var project = new Project(title, description);
         projectRepository.saveProject(project);
+        // } else if(action.equals("edit")) {
+        // Edit with repository
+        // } else if(action.equals("delete")) {
+        // Delete with repository
+        // }
 
         response.sendRedirect(request.getContextPath() + "/projects");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-
         action = (action == null) ? "index" : action;
 
         if (action.equals("create")) {
             showCreateForm(request, response);
-        } else {
+        }
+        else if(action.equals("edit")){
+            // showEditForm(request, response)
+        }
+        else {
             listProjects(request, response);
         }
     }
@@ -55,5 +69,24 @@ public class ProjectsServlet extends HttpServlet {
     private void showCreateForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/views/projects/create.jsp");
         dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // BASE_URL = http://localhost:8080/assignment_jakarta_ee_war_exploded
+        // {BASE_URL}/projects?action=edit&id=X
+
+        // Get current id
+        // var idString = request.getParameter("id");
+        // var id = (TO LONG)
+
+        // Get one project
+        // var project = projectRepository.getById(id)
+
+        // Send project to view
+        // request.setAttribute("project", project);
+
+        // Show correct view
+        // RequestDispatcher dispatcher = request.getRequestDispatcher("/views/projects/edit.jsp");
+        // dispatcher.forward(request, response);
     }
 }
